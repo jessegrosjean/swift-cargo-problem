49e9995de31b5fdfda930d772d46048ce55a772e
Jesse Grosjean <jesse@hogbaysoftware.com>
2023-09-09 09:40:58 -0400

    init

---
+++ App/App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Jesse Grosjean on 9/9/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
